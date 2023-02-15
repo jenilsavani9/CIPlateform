b58454e12d779138ed5e82938e436723bc6c1e28
Jenil Savani <jenilsavani1@gmail.com>
2023-02-15 09:43:37 +0530

    register page in MVC UI

--- MVC/CI/CIWeb/Controllers/HomeController.cs
+++ MVC/CI/CIWeb/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace CIWeb.Controllers
             return View();
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
