49c6bac8ac48ac38434311edb0a1768196b6391f
Jenil Savani <jenilsavani1@gmail.com>
2023-02-16 18:02:28 +0530

    some design changes

---
+++ MVC/CI/CIWeb/ViewModels/Skill.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CIWeb.ViewModels;
+
+public partial class Skill
+{
+    public long SkillId { get; set; }
+
+    public string? SkillName { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public virtual ICollection<MissionSkill> MissionSkills { get; } = new List<MissionSkill>();
+
+    public virtual ICollection<UserSkill> UserSkills { get; } = new List<UserSkill>();
+}
