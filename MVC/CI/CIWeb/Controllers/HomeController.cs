0e70ea4cc2b581a0c73883478a729766fcc8b40d
Jenil Savani <jenilsavani1@gmail.com>
2023-02-16 14:09:27 +0530

    landing page MVC UI

--- MVC/CI/CIWeb/Controllers/HomeController.cs
+++ MVC/CI/CIWeb/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace CIWeb.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
