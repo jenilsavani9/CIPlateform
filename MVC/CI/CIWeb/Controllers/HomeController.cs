8f65ebff9b8950eba8b38483b3e4300e0dc1777a
jenil savani <jenilsavani1@gmail.com>
2023-02-20 17:55:30 +0530

    better filter and not found page

--- MVC/CI/CIWeb/Controllers/HomeController.cs
+++ MVC/CI/CIWeb/Controllers/HomeController.cs
@@ -43,6 +43,11 @@ namespace CIWeb.Controllers
             return View();
         }
 
+        public IActionResult ErrorNotFound()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
