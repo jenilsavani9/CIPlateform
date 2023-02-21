c5d84e6d4209ab822eedd93809f08ced2cdd116d
jenil savani <jenilsavani1@gmail.com>
2023-02-21 17:49:41 +0530

    volunteer page

---
+++ MVC/CI/CIWeb/Controllers/VolunteerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CIWeb.Controllers
+{
+    public class VolunteerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
