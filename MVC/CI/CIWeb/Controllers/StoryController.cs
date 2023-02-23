6f330bd2f96cdd552362617e7649ca9566dfe37c
jenil savani <jenilsavani1@gmail.com>
2023-02-23 16:42:03 +0530

    story page

---
+++ MVC/CI/CIWeb/Controllers/StoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CIWeb.Controllers
+{
+    public class StoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
